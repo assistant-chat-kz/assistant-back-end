608313cfe4d3cc35402c596cb67bb266327f6b0c
Ildar Gazizov <vito.scalepta@gmail.com>
2025-02-14 19:28:05 +0500

    feat: add get user by id

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,16 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { YandexGptService } from './services/yandex-gpt.service';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
 import { YandexGptController } from './yandex-gpt/yandex-gpt.controller';
+import { PrismaService } from './prisma.service'
+
 
 @Module({
   imports: [AuthModule],
-  controllers: [AppController, YandexGptController],
-  providers: [AppService, YandexGptService],
+  controllers: [AppController, YandexGptController, UserController],
+  providers: [AppService, YandexGptService, UserService, PrismaService],
   exports: [YandexGptService]
 })
 export class AppModule { }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Param } from "@nestjs/common";
 import { UserService } from "./user.service";
 
 @Controller('users')
 export class UserController {
-    constructor(private readonly userService: UserService) {}
+    constructor(private readonly userService: UserService) { }
 
     @Get()
     async getAll() {
         return this.userService.getAll()
+    }
+
+    @Get(':id')
+    async getUserById(@Param('id') id: string) {
+        return this.userService.getUserById(id);
     }
 }
