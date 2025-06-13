ba8be820f700bfaccc84d8ce128fd9fb41299ae4
Ildar Gazizov <vito.scalepta@gmail.com>
2025-06-13 18:02:42 +0500

    feat: add cors for domen aikouch.kz

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3001', 'http://147.182.189.56'],
+    origin: ['http://localhost:3000', 'http://localhost:3001', 'http://147.182.189.56', 'http://aikouch.kz', 'http://www.aikouch.kz'],
     credentials: true,
   });
 
