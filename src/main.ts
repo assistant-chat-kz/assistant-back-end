2860d520320de4bf64c6ac574e929f34c65f10a3
Ildar Gazizov <110162487+MartinkaOff@users.noreply.github.com>
2025-07-03 14:17:36 +0500

    feat: add https for domen

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
 
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3001', 'http://147.182.189.56', 'http://aikouch.kz', 'http://www.aikouch.kz'],
+    origin: ['http://localhost:3000', 'http://localhost:3001', 'https://147.182.189.56', 'https://aikouch.kz', 'https://www.aikouch.kz'],
     credentials: true,
   });
 
